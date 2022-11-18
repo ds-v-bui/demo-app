233cc393f1bdf413e0f2d9011d0b5b10004e5e5e
Viet Bui Duc <ducviet.dev@gmail.com>
2022-11-18 08:59:16 +0700

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    roots: ['__tests__'],
+    testMatch: ['**/*.+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is a demo microservice');
+});
+
+app.listen(8080, () => {
+    console.log('Example app listening on port 80!');
+});
